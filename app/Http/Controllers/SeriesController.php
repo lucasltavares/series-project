a914758646dfa52eb4d67f5798e66c82d912c5aa
lucas.lima <lucas@tew.one>
2025-02-24 14:23:26 -0300

    ok

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -47,6 +47,7 @@ class SeriesController extends Controller
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         $schedule = now()->addSeconds(5);
 
         foreach ($users as $user) {
@@ -55,6 +56,10 @@ class SeriesController extends Controller
         foreach ($users as $user) {
             Mail::to($user)->send(new SeriesCreated(
 >>>>>>> 9d57ce86bcf99c6616e734e676b27eb30de89eb3
+=======
+        foreach ($users as $user) {
+            Mail::to($user)->send(new SeriesCreated(
+>>>>>>> origin/main
 =======
         foreach ($users as $user) {
             Mail::to($user)->send(new SeriesCreated(
@@ -86,12 +91,16 @@ class SeriesController extends Controller
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             );
 
             Mail::to($user)->later($schedule, $email);
 =======
             ));
 >>>>>>> 9d57ce86bcf99c6616e734e676b27eb30de89eb3
+=======
+            ));
+>>>>>>> origin/main
 =======
             ));
 >>>>>>> origin/main
